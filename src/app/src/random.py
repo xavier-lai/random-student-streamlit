baca820cd3f04c29dbe58246ab552d865200836c
Xavier Lai <xavier.l@cabaia.fr>
2025-11-19 14:27:20 +0100

    feat(app) : enable random choose to get the group order passage

--- src/app/src/random.py
+++ src/app/src/random.py
@@ -29,3 +29,9 @@ def get_random_student(
     selected_students.update(chosen_students)
 
     return chosen_students
+
+
+def get_order_order(number_of_groups: int):
+    order = list(range(number_of_groups))
+    random.shuffle(order)
+    return order
